a3d718d3e4ea0e71432384228f6380ba372b1da2
someoneatemylastsliceofpizza <76086048+someoneatemylastsliceofpizza@users.noreply.github.com>
2025-05-18 00:26:22 +0700

    Update setrefzero.py

--- scripts/setrefzero.py
+++ scripts/setrefzero.py
@@ -16,7 +16,7 @@ for bone in anim:
     anims.append(bone.split("\n")[1:-1])
 
 for bone in range(len(anims[0])):
-    if bone not in [8, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113]: #
+    if bone not in [8, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113]: # custom/weapon bones
         rawv = anims[0][bone].split()[1:]
         calcrot = ' '.join("0.000000" for i in range(3))
         anims[0][bone] = f"{bone}  {' '.join(rawv[:3])}  {calcrot}"
